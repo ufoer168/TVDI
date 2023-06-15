739a9bc850034f8aa730756c689c1a57f472fedf
ufoer168 <ufoer168@gmail.com>
2023-06-15 16:07:21 +0800

    20230615

--- Python/lesson3_2.py
+++ Python/lesson3_2.py
@@ -1,16 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
 from gpiozero import LED
+import datasource
 
-class Window(tk.Tk):    
-    def __init__(self, led):
-        super().__init__()
+class LEDButton(ttk.Button):
+    def __init__(self,master,led,**kwargs):
+        super().__init__(master,**kwargs)
+        self.led = led
         self.state = False
-        self.title('這是我的第一個視窗')
-        self.resizable(False, False)
+        self.configure(command=self.user_click)
         s = ttk.Style()
         s.theme_use('clam')
-        s.configure('Title.TLabel',font=('Arial',20))
         s.configure('LEDClose.TButton',                    
                     font=('Arial',20),
                     borderwidth=5,
@@ -22,25 +22,44 @@ class Window(tk.Tk):
                     borderwidth=5,
                     padding=(10,20),
                     )
+    
+    def user_click(self):
+        self.state = not self.state
+        if self.state:
+            self.configure(text='LED 關')
+            self.configure(style='LEDOpen.TButton')
+            self.led.on()
+            datasource.insert_data(1)
+        else:
+            self.configure(text='LED 開')
+            self.configure(style='LEDClose.TButton')
+            self.led.off()
+            datasource.insert_data(0)
+    
+
+class Window(tk.Tk):    
+    def __init__(self,redLed,**kwargs):
+        '''
+        @parmater redLed,是gpiozeor.LED的實體
+        '''
+        super().__init__(**kwargs)       
+        
+        self.title('這是我的第一個視窗')
+        self.resizable(False, False)
+        s = ttk.Style()
+        s.theme_use('clam')
+        s.configure('Title.TLabel',font=('Arial',20))        
         #pprint.pprint(s.layout('TButton'))
         #pprint.pprint(s.element_options('Button.focus'))
         title_label = ttk.Label(self,text="LED控制器",style='Title.TLabel')        
         title_label.pack(pady=25,padx=100)
 
-        self.led_btn = ttk.Button(self,text="LED 開",style='LEDClose.TButton',command=self.user_click(led))             
+        self.led_btn = LEDButton(self,led=redLed,text="LED 開",style='LEDClose.TButton')             
         self.led_btn.pack(pady=(10,50))
 
-    def user_click(self, led):
-        self.state = not self.state
-        if self.state:
-            self.led_btn.configure(text='LED 關')
-            self.led_btn.configure(style='LEDOpen.TButton')
-            led.on()
-        else:
-            self.led_btn.configure(text='LED 開')
-            self.led_btn.configure(style='LEDClose.TButton')
-            led.off()
 
-if __name__ == "__main__":
-    window = Window(LED(23))   
+if __name__ == "__main__":   
+    led = LED(23)
+    led.off()
+    window = Window(led)   
     window.mainloop()
---
+++ Python/stock.py
@@ -0,0 +1,4 @@
+import webview
+
+webview.create_window("盯股幫", "", width=850, height=600, text_select=False)
+webview.start()
